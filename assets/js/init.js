86ea48a5189db3031a685255046663af9a0734d8
Eiston Wei <eiston.wei@gmail.com>
2017-06-12 20:32:50 -0400

    login

---
+++ assets/js/init.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyBiqXIaSad119OT8esJHGxxSOkMx8cBJA0",
+    authDomain: "gosco-web.firebaseapp.com",
+    databaseURL: "https://gosco-web.firebaseio.com",
+    projectId: "gosco-web",
+    storageBucket: "",
+    messagingSenderId: "491320634555"
+  };
+  firebase.initializeApp(config);
